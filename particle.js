9afcf81663e1fe4047f5484a189b97c7aa3e7872
Aisyah <123235289+Aisyah629@users.noreply.github.com>
2025-12-25 12:23:54 +0700

    Add particle.js with basic styling and layout

---
+++ particle.js
@@ -0,0 +1,22 @@
+html, body {
+      margin: 0;
+      padding: 0;
+      width: 100%;
+      height: 100%;
+    }
+
+    #particles-js {
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      background-color: #0d0d0d;
+      z-index: -1;
+    }
+
+    .content {
+      position: relative;
+      color: white;
+      text-align: center;
+      top: 40%;
+      font-size: 2rem;
+    }
